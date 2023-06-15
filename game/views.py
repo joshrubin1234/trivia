41b1412d6f03d4844e2ce8b0eb90102248a4c70d
Josh Rubin <joshrubin2014@gmail.com>
2023-06-14 22:06:37 -0400

    forms created for accounts

--- game/views.py
+++ game/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm, LoginForm
 # Create your views here.
