7e46776f5c9fea8a2656959a5b4178891245158c
Josh Rubin <joshrubin2014@gmail.com>
2023-06-22 17:23:36 -0400

    correct answer lights up green, wrong answer lights up red and score iterates correctly

--- game/views.py
+++ game/views.py
@@ -55,7 +55,10 @@ def play_view(request, category_id):
 @csrf_exempt
 def check_answer(request):
     data = json.loads(request.body)
-    answer_text = data.get('answer_text')
+    answer_text = data.get('answer_text').strip()  # Using strip() function to remove spaces from both ends
+    
+
+
 
     # Retrieve the game instance related to the current user
     game = Game.objects.filter(user=request.user).first()
@@ -64,8 +67,20 @@ def check_answer(request):
         # The game does not exist or has no current question
         return JsonResponse({'error': 'Game not found or no question to answer.'}, status=404)
 
+    # Get choices for the current question
+    choices = [game.current_question.choice_1,
+               game.current_question.choice_2,
+               game.current_question.choice_3,
+               game.current_question.choice_4]
+    
+    # Get index of answer text in choices list
+    answer_index = choices.index(answer_text) + 1  # +1 because choices are 1-indexed
+    
     # Check if the answer is correct
-    is_correct = (game.current_question.correct_choice == answer_text)
+
+    is_correct = (str(answer_index) == game.current_question.correct_choice)
+
+
 
     # Update the score if the answer is correct
     if is_correct:
@@ -85,13 +100,8 @@ def check_answer(request):
 
         correct_choice_index = int(game.current_question.correct_choice) - 1  # Get the index of the correct choice for the new question
 
-  # Get the index of the correct choice for the new question
-
-    # Send back whether the answer was correct, the new question, correct choice index, and choices
-    # Send back whether the answer was correct, the new question, correct choice index, and choices
+        # Send back whether the answer was correct, the new question, correct choice index, and choices
         return JsonResponse({'is_correct': is_correct, 'question': game.current_question.question_text, 'correct_choice_index': correct_choice_index, 'choices': choices, 'score': game.score, 'questions_answered': game.questions.count()})
     else:
         # There are no more questions
         return JsonResponse({'is_correct': is_correct, 'error': 'No more questions available.', 'score': game.score, 'questions_answered': game.questions.count()})
-
-
